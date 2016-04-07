bd496bd061bc14365e1a9fe656d7f2bd9038177e
crazyAxe <529076144@qq.com>
2016-04-08 00:23:19 +0800

    detail view done

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
 from django.utils import timezone
 from django.views import generic
+import logging
 
 
 class IndexView(generic.ListView):
@@ -13,3 +14,15 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         blogs = Blog.objects.filter(create_time__lte=timezone.now()).order_by('-create_time')
         return blogs
+
+
+class DetailView(generic.DetailView):
+    model = Blog
+    template_name = 'blog/detail.html'
+    context_object_name = 'the_blog'
+
+
+# def detail(request, pk):
+#     the_blog = Blog.objects.get(id=pk)
+#     context = {'the_blog': the_blog}
+#     return render(request, 'blog/detail.html', context)
